3933a9bf6cf9cc9524da1c5a2cce70ef2168a093
dttncl <82695034+dttncl@users.noreply.github.com>
2024-01-03 15:46:08 -0500

    modified list generator from sync to async

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({ 
   selector: 'app-heroes',
@@ -24,5 +25,29 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm'
   };*/
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {
+    // not the best practice
+    // use ngOnInit instead
+    // this.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    //generate list of heroes synchronously
+    //requests the list before it loads the website
+    //this.heroes = this.heroService.getHeroes();
+
+    //generate list of heroes ASYNCHRONOUSLY
+    //waits for the Observable to produce the array of heroes
+    //subscriber passes the generated list then sets the component's list of heroes
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  
+  }
+
 }
