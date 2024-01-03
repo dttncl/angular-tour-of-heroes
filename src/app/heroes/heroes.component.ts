8c161dfe9d47c399e88adf73c3427a335204d834
dttncl <82695034+dttncl@users.noreply.github.com>
2024-01-03 12:17:15 -0500

    added list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { HEROES } from '../mock-heroes';
 
 import { Hero } from '../hero';
 
 @Component({ 
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule,  FormsModule],
+  imports: [CommonModule,  FormsModule, NgFor],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 
 export class HeroesComponent {
   //hero = 'Windstorm';
-  hero : Hero = {
+  /*hero : Hero = {
     id: 1,
     name: 'Windstorm'
-  };
+  };*/
+  heroes = HEROES;
 }
