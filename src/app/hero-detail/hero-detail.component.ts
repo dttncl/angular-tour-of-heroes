f07645d84225618e155c11bd9d34184335ef2671
dttncl <82695034+dttncl@users.noreply.github.com>
2024-01-03 13:32:43 -0500

    modified heroes component to use hero-details component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,  FormsModule, NgFor],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
-
 import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({ 
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule,  FormsModule, NgFor],
+  imports: [CommonModule,  FormsModule, NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
