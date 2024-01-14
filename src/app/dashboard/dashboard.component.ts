b0cf7ce3d29c9fb0baad17232742a8e3c2045f4c
dttncl <82695034+dttncl@users.noreply.github.com>
2024-01-13 19:49:34 -0500

    added dashboard css

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,13 +4,14 @@ import { RouterModule } from '@angular/router';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { HeroSearchComponent } from "../hero-search/hero-search.component";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.css',
+    imports: [CommonModule, RouterModule, HeroSearchComponent]
 })
 
 export class DashboardComponent implements OnInit {
